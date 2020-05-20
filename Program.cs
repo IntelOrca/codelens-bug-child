4b13ff703ecc8f6e94a610a97f8fea064b62b335
Ted John <ted@brambles.org>
2020-05-20 13:24:53 +0100

    Add more code to main method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace codelensbug
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
